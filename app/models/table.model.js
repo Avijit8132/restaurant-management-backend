7b45685d4a7f0dd8b97df9e5d7e9f9e407bfdc53
Avijit8132 <avijit.b@ibirdsservices.com>
2024-02-16 18:23:39 +0530

    table insert

---
+++ app/models/table.model.js
@@ -0,0 +1,93 @@
+const sql = require("./db.js");
+
+let schema = "";
+
+function init(schema_name) {
+  this.schema = schema_name;
+}
+
+async function create(newTable, userid) {
+  console.log('-----create task model--------');
+  delete newTable.id;
+  console.log("this.schema", this.schema);
+
+  const result = await sql.query(
+    `INSERT INTO ${this.schema}.table
+     (occupancy, status, description, lastmodifiedbyid, createdbyid)  
+     VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+    [newTable.occupancy, newTable.status, newTable.description, userid, userid]
+  );
+
+  if (result.rows.length > 0) {
+    return { id: result.rows[0].id, ...newTable };
+  }
+
+  return null;
+}
+
+async function findById(id) {
+  console.log("id ", id);
+
+  const result = await sql.query(`SELECT * FROM ${this.schema}.table WHERE id = $1`, [id]);
+
+  console.log("Rows ", result.rows);
+  if (result.rows.length > 0)
+    return result.rows[0];
+
+  return null;
+};
+
+async function findAll() {
+  const result = await sql.query(`SELECT * FROM ${this.schema}.table`);
+  console.log('rows:===>', result.rows);
+  return result.rows;
+};
+
+async function updateById(id, newTable, userid) {
+  // Implementation of the updateById function
+  console.log('===Model Table Update====');
+  delete newTable.id;
+  newTable['lastmodifiedbyid'] = userid;
+
+  const query = buildUpdateQuery(id, newTable, this.schema);
+
+  // Turn newTable into an array of values
+  var colValues = Object.values(newTable);
+
+  console.log('query:', query);
+  const result = await sql.query(query, colValues);
+  if (result.rowCount > 0) {
+    return { "id": id, ...newTable };
+  }
+  return null;
+}
+
+async function deleteTask(id) {
+  const result = await sql.query(`DELETE FROM ${this.schema}.table WHERE id = $1`, [id]);
+
+  if (result.rowCount > 0)
+    return "Success"
+  return null;
+};
+
+function buildUpdateQuery(id, cols, schema) {
+  // Setup static beginning of query
+  var query = [`UPDATE ${schema}.table`];
+  query.push('SET');
+
+  // Create another array storing each set command
+  // and assigning a number value for parameterized query
+  var set = [];
+  Object.keys(cols).forEach(function (key, i) {
+    set.push(key + ' = ($' + (i + 1) + ')');
+  });
+  query.push(set.join(', '));
+
+  // Add the WHERE statement to look up by id
+  query.push('WHERE id = $' + (Object.keys(cols).length + 1));
+
+  // Return a complete query string
+  return query.join(' ');
+}
+
+module.exports = { init, findById, updateById, findAll, create, deleteTask };
---
+++ app/routes/table.routes.js
@@ -0,0 +1,116 @@
+const e = require("express");
+const { fetchUser } = require("../middleware/fetchuser.js");
+const Table = require("../models/table.model.js"); 
+const permissions = require("../constants/permissions.js");
+
+module.exports = app => {
+  
+  const { body, validationResult } = require('express-validator');
+  var router = require("express").Router();
+
+  // Create a new Table
+  router.post("/", fetchUser, [
+    body('occupancy', 'Please enter occupancy').isLength({ min: 1 }),
+    body('status', 'Please enter status').isLength({ min: 1 }),
+    body('description', 'Please enter description').isLength({ min: 1 })
+    // Add validation rules for other fields as needed
+  ] ,
+  async (req, res)=>{
+    // Check permissions
+    // const permission = req.userinfo.permissions.find(el => el.name === permissions.CREATE_TABLE);
+    // if (!permission) return res.status(401).json({errors : "Unauthorized"});
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors : errors.array()});
+    }
+    
+    Table.init(req.userinfo.tenantcode);
+    const tableRec = await Table.create(req.body,  req.userinfo.id);
+    
+    if(!tableRec){
+      return res.status(400).json({errors : "Bad Request"});
+    }
+
+    return res.status(201).json(tableRec);
+  });
+
+  // Get All Tables
+  router.get("/", fetchUser, async (req, res)=>{
+    // Check permissions
+    // const permission = req.userinfo.permissions.find(el => el.name === permissions.VIEW_TABLES);
+    // if (!permission) return res.status(401).json({errors : "Unauthorized"});
+
+    Table.init(req.userinfo.tenantcode);
+    const tables = await Table.findAll();
+    if(tables){
+      res.status(200).json(tables);
+    }else{
+      res.status(400).json({errors : "No data"});
+    }
+  });
+
+  // Get Table by ID
+  router.get("/:id", fetchUser, async (req, res)=>{
+    // Check permissions
+    // const permission = req.userinfo.permissions.find(el => el.name === permissions.VIEW_TABLE);
+    // if (!permission) return res.status(401).json({errors : "Unauthorized"});
+
+    Table.init(req.userinfo.tenantcode);
+    const table = await Table.findById(req.params.id);
+    if(table){
+      res.status(200).json(table);
+    }else{
+      res.status(400).json({errors : "No data"});
+    }
+  });
+
+
+  router.put("/:id", fetchUser, async (req, res) => {
+    try {
+      //Check permissions
+      const permission = req.userinfo.permissions.find(el => el.name === permissions.EDIT_TABLE || el.name === permissions.MODIFY_ALL);
+      if (!permission) return res.status(401).json({errors : "Unauthorized"});
+
+      const { occupancy, status, description } = req.body;
+      const errors = [];
+      const tableRec = {};
+
+      if (req.body.hasOwnProperty("occupancy")) { tableRec.occupancy = occupancy; }
+      if (req.body.hasOwnProperty("status")) { tableRec.status = status; }
+      if (req.body.hasOwnProperty("description")) { tableRec.description = description; }
+
+      if (errors.length !== 0) {
+        return res.status(400).json({errors : errors});
+      }
+
+      Table.init(req.userinfo.tenantcode);
+      const result = await Table.updateById(req.params.id, tableRec, req.userinfo.id);
+
+      if (result) {
+        return res.status(200).json(result);
+      } else {
+        return res.status(404).json({errors: "Record not found"});
+      }
+    } catch (error) {
+      res.status(500).json({errors : error});
+    }
+  });
+
+
+  // Delete Table by ID
+  router.delete("/:id", fetchUser, async (req, res) => {
+    // Check permissions
+    // const permission = req.userinfo.permissions.find(el => el.name === permissions.DELETE_TABLE);
+    // if (!permission) return res.status(401).json({errors : "Unauthorized"});
+
+    Table.init(req.userinfo.tenantcode);
+    const result = await Table.deleteTask(req.params.id);
+    if(!result)
+      return res.status(200).json({"success" : false, "message"  : "No record found"});
+    
+    res.status(200).json({"success" : true, "message"  : "Successfully Deleted"});
+  });
+
+  app.use(process.env.BASE_API_URL + '/api/tables', router);
+};
