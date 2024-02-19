8bfbeff23ee8ca056ee8507dedb6a9c251bbf468
Avijit8132 <avijit.b@ibirdsservices.com>
2024-02-19 12:04:19 +0530

    new update

---
+++ app/models/booking.model.js
@@ -0,0 +1,128 @@
+const sql = require("./db.js");
+
+let schema = "";
+
+function init(schema_name) {
+  this.schema = schema_name;
+}
+
+async function create(newBooking, userid) {
+  delete newBooking.id;
+
+  const result = await sql.query(
+    `INSERT INTO ${this.schema}.booking 
+     (tableid, contactid, status, numberofperson, lastmodifiedbyid, createdbyid, createddate, lastmodifieddate)  
+     VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
+    [
+      newBooking.tableid,
+      newBooking.contactid,
+      newBooking.status,
+      newBooking.numberofperson,
+      userid,
+      userid,
+      newBooking.createddate,
+      newBooking.lastmodifieddate
+    ]
+  );
+
+  if (result.rows.length > 0) {
+    return { id: result.rows[0].id, ...newBooking };
+  }
+
+  return null;
+}
+
+async function findById(id) {
+  const query = `SELECT * FROM ${this.schema}.booking WHERE id = $1`;
+  const result = await sql.query(query, [id]);
+
+  if (result.rows.length > 0) {
+    return result.rows[0];
+  }
+
+  return null;
+}
+
+async function findAll() {
+  const query = `SELECT * FROM ${this.schema}.booking ORDER BY createddate DESC`;
+  const result = await sql.query(query);
+  return result.rows;
+}
+
+
+async function updateById(id, newBooking, userid) {
+    console.log('===Model Booking Update====');
+    delete newBooking.id;
+    newBooking['lastmodifiedbyid'] = userid;
+  
+    const query = buildUpdateQuery(id, newBooking, this.schema);
+    
+    // Extract the values from newBooking object
+    const colValues = Object.keys(newBooking).map(key => newBooking[key]);
+  
+    // Add the id parameter at the end of colValues array
+    colValues.push(id);
+  
+    console.log('query:', query);
+    console.log('colValues:', colValues);
+  
+    const result = await sql.query(query, colValues);
+    if (result.rowCount > 0) {
+      return { "id": id, ...newBooking };
+    }
+    return null;
+  }
+  
+  
+async function deleteBooking(id) {
+  const result = await sql.query(
+    `DELETE FROM ${this.schema}.booking WHERE id = $1`,
+    [id]
+  );
+
+  if (result.rowCount > 0) return "Success";
+  return null;
+}
+
+// function buildUpdateQuery(id, cols, schema) {
+//   let query = [`UPDATE ${schema}.booking SET`];
+
+//   const set = [];
+//   Object.keys(cols).forEach((key, i) => {
+//     set.push(`${key} = ($${i + 1})`);
+//   });
+//   query.push(set.join(", "));
+
+//   query.push("WHERE id = $");
+//   query.push(id);
+
+//   return query.join(" ");
+// }
+function buildUpdateQuery(id, cols, schema) {
+    // Setup static beginning of query
+    var query = `UPDATE ${schema}.booking SET`;
+  
+    // Create another array storing each set command
+    // and assigning a number value for parameterized query
+    var set = [];
+    Object.keys(cols).forEach(function (key, i) {
+      set.push(`${key} = $${i + 1}`); 
+    });
+    query += ` ${set.join(', ')}`;
+  
+    // Add the WHERE statement to look up by id
+    query += ` WHERE id = $${Object.keys(cols).length + 1}`;
+  
+    // Return a complete query string
+    return query;
+  }
+  
+
+module.exports = {
+  init,
+  findById,
+  updateById,
+  findAll,
+  create,
+  deleteBooking
+};
---
+++ app/routes/booking.routes.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const { fetchUser } = require("../middleware/fetchuser.js");
+const Booking = require("../models/booking.model.js");
+const permissions = require("../constants/permissions.js");
+
+module.exports = app => {
+  const { body, validationResult } = require("express-validator");
+
+  const router = express.Router();
+
+  // Create a new Booking
+  router.post("/", fetchUser, [
+    body("tableid", "Please provide a table ID").notEmpty(),
+    body("contactid", "Please provide a contact ID").notEmpty(),
+    body("status", "Please provide a status").notEmpty(),
+    body("numberofperson", "Please provide the number of persons").notEmpty().isNumeric(),
+  ], async (req, res) => {
+    // Check permissions
+    const permission = req.userinfo.permissions.find(el => el.name === permissions.CREATE_BOOKING || el.name === permissions.MODIFY_ALL);
+    if (!permission) return res.status(401).json({ errors: "Unauthorized" });
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    Booking.init(req.userinfo.tenantcode);
+    const booking = await Booking.create(req.body, req.userinfo.id);
+
+    if (!booking) {
+      return res.status(400).json({ errors: "Bad Request" });
+    }
+
+    return res.status(201).json(booking);
+  });
+
+  // Get All Bookings
+  router.get("/", fetchUser, async (req, res) => {
+    // Check permissions
+    const permission = req.userinfo.permissions.find(el => el.name === permissions.VIEW_BOOKING || el.name === permissions.VIEW_ALL || el.name === permissions.MODIFY_ALL);
+    if (!permission) return res.status(401).json({ errors: "Unauthorized" });
+
+    Booking.init(req.userinfo.tenantcode);
+    const bookings = await Booking.findAll();
+    
+    if (bookings.length > 0) {
+      res.status(200).json(bookings);
+    } else {
+      res.status(404).json({ errors: "No data found" });
+    }
+  });
+
+  // Get Booking by ID
+  router.get("/:id", fetchUser, async (req, res) => {
+    // Check permissions
+    const permission = req.userinfo.permissions.find(el => el.name === permissions.VIEW_BOOKING || el.name === permissions.VIEW_ALL || el.name === permissions.MODIFY_ALL);
+    if (!permission) return res.status(401).json({ errors: "Unauthorized" });
+
+    Booking.init(req.userinfo.tenantcode);
+    const booking = await Booking.findById(req.params.id);
+
+    if (booking) {
+      return res.status(200).json(booking);
+    } else {
+      return res.status(404).json({ errors: "Booking not found" });
+    }
+  });
+
+  // Update Booking
+  router.put("/:id", fetchUser, async (req, res) => {
+    // Check permissions
+    const permission = req.userinfo.permissions.find(el => el.name === permissions.EDIT_BOOKING || el.name === permissions.MODIFY_ALL);
+    if (!permission) return res.status(401).json({ errors: "Unauthorized" });
+
+    const bookingData = req.body;
+    Booking.init(req.userinfo.tenantcode);
+    const updatedBooking = await Booking.updateById(req.params.id, bookingData, req.userinfo.id);
+
+    if (updatedBooking) {
+      return res.status(200).json({ success: true, message: "Booking updated successfully" });
+    } else {
+      return res.status(404).json({ errors: "Booking not found" });
+    }
+  });
+
+  // Delete Booking
+  router.delete("/:id", fetchUser, async (req, res) => {
+    // Check permissions
+    const permission = req.userinfo.permissions.find(el => el.name === permissions.DELETE_BOOKING || el.name === permissions.MODIFY_ALL);
+    if (!permission) return res.status(401).json({ errors: "Unauthorized" });
+
+    Booking.init(req.userinfo.tenantcode);
+    const result = await Booking.deleteBooking(req.params.id);
+
+    if (result) {
+      return res.status(200).json({ success: true, message: "Booking deleted successfully" });
+    } else {
+      return res.status(404).json({ errors: "Booking not found" });
+    }
+  });
+
+  app.use(process.env.BASE_API_URL + '/api/bookings', router);
+};
